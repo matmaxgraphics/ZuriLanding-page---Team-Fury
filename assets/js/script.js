d256c89eb37a9588ccffe814c63acda63e484026
deecodez <obarodayo@gmail.com>
2021-04-12 03:33:38 +0100

    landing page implementation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,34 @@
+jQuery(document).ready(function ($) {
+
+    // Header fixed on scroll
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        $('#header').addClass('header-scrolled');
+      } else {
+        $('#header').removeClass('header-scrolled');
+      }
+    });
+  
+    if ($(window).scrollTop() > 100) {
+      $('#header').addClass('header-scrolled');
+    }
+  
+
+
+  });
+
+
+  
+
+  
+
+  
+  
+  const openNav = () => {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  const closeNav = () => {
+    document.getElementById("myNav").style.width = "0%";
+  }
 
